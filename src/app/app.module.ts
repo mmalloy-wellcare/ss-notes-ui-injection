0d06d387f1b540df8da650e22f5e5db0989aa46c
Rendon <SRENDON@wellcare.com>
2020-09-18 07:37:03 -0500

    PBP-4674 - notes for invoices - Spencer Rendon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { NgModule, DoBootstrap, Injector } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { createCustomElement } from '@angular/elements';
+import { NotesInvoiceComponent } from './components/notes-invoice/notes-invoice.component';
+import { NotesService } from './services/notes.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AlertsService } from '@nextgen/web-care-portal-core-library';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesInvoiceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  providers: [NotesService, AlertsService],
   bootstrap: [],
-  entryComponents: [AppComponent]
+  entryComponents: [AppComponent, NotesInvoiceComponent]
 })
 
 
@@ -23,6 +31,9 @@ export class AppModule implements DoBootstrap {
 
   ngDoBootstrap() {
     const applicationWebComponent = createCustomElement(AppComponent, { injector: this.injector });
+    const notesInvoiceComponent = createCustomElement(NotesInvoiceComponent, {injector: this.injector});
+
     customElements.define('ss-notes-ui', applicationWebComponent);
+    customElements.define('ss-notes-ui-notes-invoice-cmpnt', notesInvoiceComponent);
 }
 }
---
+++ src/app/services/notes.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Sort } from '@nextgen/web-care-portal-core-library';
+import { DataService } from '@nextgen/web-care-portal-core-library';
+
+@Injectable()
+export class NotesService {
+  private pageSize = 100;
+  private notesURI = '/shared-services/ss-notes/v1/ss/notes';
+
+  constructor(private dataService: DataService) { }
+
+  public getNotes(restartRowId: string, sort: Array<Sort>, customHeaders) {
+    return this.dataService.getAllRecords(this.notesURI, this.pageSize, restartRowId, sort, customHeaders);
+  }
+}
